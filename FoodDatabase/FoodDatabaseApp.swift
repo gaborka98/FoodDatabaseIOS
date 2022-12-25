12e3ec13259c3b2d7c0b90114b4b78f1eb2212a2
Gabor Horvath <gaborka98@freemail.hu>
2022-12-25 19:30:38 +0100

    Initial Commit

---
+++ FoodDatabase/FoodDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDatabaseApp.swift
+//  FoodDatabase
+//
+//  Created by Gábor Horváth on 2022. 12. 25..
+//
+
+import SwiftUI
+
+@main
+struct FoodDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
